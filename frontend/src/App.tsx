5f05593137f4815857b03090ff33fdfdd037d82e
LuisC-web <72262647+LuisC-web@users.noreply.github.com>
2024-11-27 13:30:52 -0500

    Config Views Editor and Visor Pdf

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,26 @@
-function App() {
-  return <></>;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LayoutIndex from "./layouts/index/LayoutIndex";
+import HomeView from "./views/home/HomeView";
+import LayoutEditor from "./layouts/editor/LayoutEditor";
+import EditorView from "./views/editor/EditorView";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<LayoutIndex></LayoutIndex>}>
+          <Route path="/" element={<HomeView></HomeView>} index></Route>
+        </Route>
+        <Route element={<LayoutEditor></LayoutEditor>}>
+          <Route
+            path="/editor"
+            element={<EditorView></EditorView>}
+            index
+          ></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
